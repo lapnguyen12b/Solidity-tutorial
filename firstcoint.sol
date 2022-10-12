eb8915dbef5e62f19a6f893d50c5c6b06a94a378
lapnv <lapnguyen12b@gmail.com>
2022-10-12 23:05:44 +0700

    transfers

---
+++ firstcoint.sol
@@ -0,0 +1,25 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract FirstCoin {
+  address public minter;
+  mapping (address => uint) public balances;
+
+  event sent(address from, address to, uint amount);
+
+  constructor () {
+    minter = msg.sender;
+  }
+
+  function mint(address receiver, uint amount) public {
+    require(msg.sender == minter);
+    require(amount < 1e60);
+    balances[receiver] += amount;
+  }
+
+  function send(address receiver, uint amount) public {
+    require(amount <= balances[msg.sender], "khong du tien");
+    balances[msg.sender] -= amount;
+    balances[receiver] += amount;
+    emit sent(msg.sender, receiver, amount);
+  }
+}
