f285960a92fae80ad9ad6f0c1b376129018dcdd0
lapnv <lapnguyen12b@gmail.com>
2022-10-18 21:58:40 +0700

    time

--- player.sol
+++ player.sol
@@ -12,18 +12,26 @@ contract Game {
     address addressPlayer;
     string fullName;
     uint age;
-    Level myLevel;
+    Level playerLevel;
     string sex;
+    uint createdAt;
   }
   
   function addPlayer(string memory fullName, uint age, string memory sex) public {
     // players = Player(fullName, age, sex);
     // playerArray.push(Player(fullName, age, sex));
-    playerMapping[msg.sender] = Player(msg.sender, fullName, age, Level.fresher, sex);
+    playerMapping[msg.sender] = Player(msg.sender, fullName, age, Level.fresher, sex, block.timestamp);
     countPlayer += 1;
   }
 
-  function getPlayerLevel(address addressPlayer) public returns (Level) {
-    return playerMapping[addressPlayer].myLevel;
+  function getPlayerLevel(address addressPlayer) public view returns (Level) {
+    return playerMapping[addressPlayer].playerLevel;
+  }
+
+  function changePlayerLevel(address playerAddress) public {
+    Player storage newPlayer = playerMapping[playerAddress];
+    if (block.timestamp >= newPlayer.createdAt + 15) {
+      newPlayer.playerLevel = Level.junior;
+    }
   }
 }
